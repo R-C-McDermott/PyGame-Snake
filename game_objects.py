53f631aa5b711d509d36ac40bb15810c7cad5a4b
R-C-McDermott <ryan.c.mcdermott4@gmail.com>
2021-10-05 15:53:10 +0100

    Initial pygame window and addition of object file

---
+++ game_objects.py
@@ -0,0 +1,9 @@
+# Snake and fruit objects
+
+class Snake:
+    def __init__(self):
+        pass
+
+class Fruit:
+    def __init__(self):
+        pass
--- main.py
+++ main.py
@@ -1,3 +1,30 @@
 # Snake game using the PyGame library for python
 #
 # By Ryan C. McDermott
+
+import pygame
+from game_objects import Snake
+from game_objects import Fruit
+
+
+WIDTH, HEIGHT = 800, 600
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Snake v1.0")
+
+FPS = 60
+
+
+
+def main():
+
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+    pygame.quit()
+
+if __name__ == '__main__':
+    main()
