2c309b8a25e24290e582c7948df75923cbc7d2a4
R-C-McDermott <ryan.c.mcdermott4@gmail.com>
2021-10-15 17:20:32 +0100

    Initial construction of window and retrieving textures from sprite sheet

--- main.py
+++ main.py
@@ -3,19 +3,35 @@
 # By Ryan C. McDermott
 
 import pygame
+pygame.init()
 from game_objects import Snake
 from game_objects import Fruit
+from spritesheet import SpriteSheet
 
 
 WIDTH, HEIGHT = 800, 600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Snake v1.0")
+BACKGROUND = (50, 50, 50)
+SNAKE_WIDTH = 48
+SNAKE_HEIGHT = 48
 
 FPS = 60
 
+sprite_sheet = pygame.image.load("assets/Textures.png").convert_alpha()
 
+def draw_game(snake):
+    snake_head = SpriteSheet(sprite_sheet, 384, 384, 2, 0).get_image(8)
+    WIN.blit(snake_head, ((WIDTH // 2) - (SNAKE_WIDTH // 2), (HEIGHT // 2) - (SNAKE_HEIGHT // 2)))
+    pygame.display.update()
 
 def main():
+    snake = pygame.Rect((WIDTH // 2) - (SNAKE_WIDTH // 2),
+                        (HEIGHT // 2) - (SNAKE_HEIGHT // 2),
+                        SNAKE_WIDTH,
+                        SNAKE_HEIGHT)
+    WIN.fill(BACKGROUND)
+    pygame.display.update()
 
     clock = pygame.time.Clock()
     run = True
@@ -24,6 +40,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
+        draw_game(snake)
     pygame.quit()
 
 if __name__ == '__main__':
--- spritesheet.py
+++ spritesheet.py
@@ -1,3 +1,25 @@
 # spritesheet file containing spritesheet class
 #
 # textures obtained from https://opengameart.org/content/snake-graphics
+
+import pygame
+
+class SpriteSheet:
+    def __init__(self, textures, pic_width, pic_height, x_block, y_block):
+        self.textures = textures
+        self.pic_width = pic_width
+        self.pic_height = pic_height
+        self.x_block = x_block
+        self.y_block = y_block
+        self.texture_width = self.x_block * self.pic_width
+        self.texture_height = self.y_block * self.pic_height
+
+    def get_image(self, down_scale):
+        image = pygame.Surface((self.pic_width, self.pic_height)).convert_alpha()
+        image.blit(self.textures, (0, 0), (self.texture_width,
+                                           self.texture_height,
+                                           self.pic_width,
+                                           self.pic_height))
+        image = pygame.transform.scale(image, (self.pic_width // down_scale, self.pic_height // down_scale))
+        return image
+
