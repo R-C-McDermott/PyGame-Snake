c01bbda588c681ecfc1dd6c38e1512ad652be06e
R-C-McDermott <ryan.c.mcdermott4@gmail.com>
2021-10-25 18:28:52 +0100

    Addition of displaying score feature

--- main.py
+++ main.py
@@ -12,21 +12,25 @@ SNAKE_WIDTH = 24
 SNAKE_HEIGHT = 24
 W_MULT = 32
 H_MULT = 24
-WIDTH, HEIGHT = W_MULT*SNAKE_WIDTH, H_MULT*SNAKE_HEIGHT
+HEIGHT_BORDER = H_MULT*SNAKE_HEIGHT
+WIDTH, HEIGHT = W_MULT*SNAKE_WIDTH, HEIGHT_BORDER + (4 * SNAKE_HEIGHT)
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Snake v1.0")
 BACKGROUND = (50, 50, 50)
-SNAKE_COLOUR = (0, 255 ,0)
+SNAKE_COLOUR = (0, 255, 0)
 FRUIT_COLOUR = (255, 0, 0)
+BORDER_COLOUR = (0, 0, 255)
+GAME_FONT = pygame.font.SysFont('Times New Roman', 30)
 FPS = 15
 
 sprite_sheet = pygame.image.load("assets/Textures.png").convert_alpha()
 LOSE_SCREEN = pygame.image.load("assets/LoseScreen.png").convert_alpha()
 LOSE_SCREEN = pygame.transform.scale(LOSE_SCREEN, (WIDTH, HEIGHT))
 
+border_line = pygame.Rect(0, HEIGHT_BORDER, WIDTH, SNAKE_HEIGHT // 2)
 
 snake = pygame.Rect((WIDTH // 2) - SNAKE_WIDTH,
-                        (HEIGHT // 2),
+                        (HEIGHT_BORDER // 2),
                         SNAKE_WIDTH,
                         SNAKE_HEIGHT)
 snake_change_x = 0
@@ -36,9 +40,13 @@ fruit_x = randrange(1, W_MULT) * SNAKE_WIDTH
 fruit_y = randrange(1, H_MULT) * SNAKE_HEIGHT
 fruit = pygame.Rect(fruit_x, fruit_y, SNAKE_WIDTH, SNAKE_HEIGHT)
 
-def draw_game(snake, fruit, snake_body):
+def draw_game(snake, fruit, snake_body, score):
     WIN.fill(BACKGROUND)
+    pygame.draw.rect(WIN, BORDER_COLOUR, (border_line.x, border_line.y, WIDTH, SNAKE_HEIGHT // 2))
     snake_head = SpriteSheet(sprite_sheet, 384, 384, 2, 0).get_image(8)
+
+    text_surface = GAME_FONT.render(f"Score: {score}", True, BORDER_COLOUR)
+    WIN.blit(text_surface, (40, HEIGHT_BORDER + 40))
 
     if len(snake_body) == 1:
         pygame.draw.rect(WIN, SNAKE_COLOUR, (snake.x, snake.y, SNAKE_WIDTH, SNAKE_HEIGHT))
@@ -65,13 +73,14 @@ def snake_move(snake, key, snake_body):
             (snake.y - SNAKE_WIDTH) not in [snake_list[1] for snake_list in snake_body]):
         snake_change_y = - SNAKE_HEIGHT
         snake_change_x = 0
-    if (key[pygame.K_DOWN] and snake.y < HEIGHT - SNAKE_HEIGHT and
+    if (key[pygame.K_DOWN] and snake.y < HEIGHT_BORDER - SNAKE_HEIGHT and
             (snake.y + SNAKE_WIDTH) not in [snake_list[1] for snake_list in snake_body]):
         snake_change_y = SNAKE_HEIGHT
         snake_change_x = 0
 
 
 def main():
+    score = 0
     snake_length = 1
     snake_body = []
     clock = pygame.time.Clock()
@@ -94,8 +103,9 @@ def main():
             fruit.x = randrange(1, W_MULT) * SNAKE_WIDTH
             fruit.y = randrange(1, H_MULT) * SNAKE_HEIGHT
             snake_length += 1
+            score += 1
 
-        draw_game(snake, fruit, snake_body)
+        draw_game(snake, fruit, snake_body, score)
 
 
         if snake.x < 0 or snake.x > (WIDTH - SNAKE_WIDTH):
@@ -105,7 +115,7 @@ def main():
             time.sleep(3)
 
 
-        if snake.y < 0 or snake.y > (HEIGHT - SNAKE_HEIGHT):
+        if snake.y < 0 or snake.y > (HEIGHT_BORDER - SNAKE_HEIGHT):
             run = False
             WIN.blit(LOSE_SCREEN, (0, 0))
             pygame.display.update()
